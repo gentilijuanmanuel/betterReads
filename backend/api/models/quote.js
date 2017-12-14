c077fbc3a0019686cc61dcdfb0fe3c2c61e5371c
Juan Manuel Gentili <gentilijuanmanuel80974a@gmail.com>
2017-12-14 15:51:59 -0300

    Models + routing + small changes

--- backend/api/models/quote.js
+++ backend/api/models/quote.js
@@ -2,8 +2,8 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var schema = new Schema({
-    titleOfTheBook: {type: string, required: true}, //no sé si esto está bien
-    description: {type: string, required: true}
+    titleOfTheBook: {type: String, required: true}, //no sé si esto está bien
+    description: {type: String, required: true}
 });
 
 module.exports = mongoose.model('Quote', schema);
--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,33 @@
+const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+
+mongoose.connect('mongodb://localhost/better-reads',{useMongoClient: true });
+require('./api/models/author.js');
+require('./api/models/book.js');
+require('./api/models/quote.js');
+require('./api/models/review.js');
+require('./api/models/user.js');
 
 const routes = require('./api/routes/index');
 
 app.use(routes);
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+//cors handling
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Accept, Authorization"
+    );
+    if(req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({});
+    }
+});
 
 /*
 * This is the error handler. Be sure to put all custom routes BEFORE this.
