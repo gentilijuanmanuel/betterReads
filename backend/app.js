7ee053555848d3dc731ab5d988ffee8b77ab7ba1
Julian Poma <poma.nob@gmail.com>
2017-12-14 11:05:41 -0300

    Basic folder setup. Initial routes

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 
+const routes = require('./api/routes/index');
+
+app.use(routes);
+
 app.use((req, res, next) => {
     res.status(200).json({
         message: 'The server is working!'
