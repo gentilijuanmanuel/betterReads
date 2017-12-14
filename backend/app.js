ad760697694ed468b18dffceea5cb67451898cd1
Julian Poma <poma.nob@gmail.com>
2017-12-14 11:41:53 -0300

    basic error handling. Updated Planification

--- backend/app.js
+++ backend/app.js
@@ -5,10 +5,24 @@ const routes = require('./api/routes/index');
 
 app.use(routes);
 
+/*
+* This is the error handler. Be sure to put all custom routes BEFORE this.
+* If any request was catched by the routes BEFORE, the following route will handle the error
+* and throw a message.
+*/
+
 app.use((req, res, next) => {
-    res.status(200).json({
-        message: 'The server is working!'
-    });
+    const error = new Error('What you are looking for is not here');
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500).json({
+        error: {
+            message: error.message
+        }
+    })
 });
 
 module.exports = app;
