b356cd6fa10659bc31c3f3f0ac4c48345c46ecde
Julian Poma <poma.nob@gmail.com>
2017-12-26 23:16:43 -0300

    Added email regex

--- backend/api/models/user.js
+++ backend/api/models/user.js
@@ -2,12 +2,15 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var userSchema = new Schema({
-    username: {type: String, required: true},
+    username: {type: String, required: true, unique:true},
     password: {type: String, required: true},
     name: {type: String, required: true},
     surname: {type: String, required: true},
     dateOfBirth: {type: String, required: true},
-    email: {type: String, required: true},
+    email: { type: String, 
+             required: true, 
+             match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
+        },
     gender: {type: String, enum:['M', 'F'], default: 'M'}
 });
 
