11f4a299bb3c2f48df24fdef7d8a695ef59f900f
Julian Poma <poma.nob@gmail.com>
2018-01-15 17:14:00 -0300

    User routes revisited

--- backend/api/models/user.js
+++ backend/api/models/user.js
@@ -5,8 +5,7 @@ var userSchema = new Schema({
     email: {
         type: String,
         required: true,
-        match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/,
-        unique: true
+        match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
     },
     password: {type: String, required: true},
     name: {type: String, required: true},
