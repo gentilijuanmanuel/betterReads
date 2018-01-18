4098fc768908dd3c18962f69e47637de435da881
Julian Poma <poma.nob@gmail.com>
2018-01-18 18:10:37 -0300

    Initial books routes

--- backend/api/routes/book.js
+++ backend/api/routes/book.js
@@ -3,11 +3,9 @@ const mongoose = require('mongoose');
 const Book = mongoose.model('Book');
 const router = express.Router();
 
-
-//return all the books
 router.get('/', (req, res, next) => {
     Book.find()
-        .select('isbn title author description')
+        .select('isbn title author description image')
         .exec()
         .then(bookCollection => {
             const response = {
@@ -31,13 +29,11 @@ router.get('/', (req, res, next) => {
 });
 
 
-//return a book
-
 router.get('/:bookId', (req, res, next) => {
     const id = req.params.bookId;
 
     Book.findById(id)
-        .select('isbn title author description')
+        .select('isbn title author description image quotes reviews')
         .exec()
         .then(result => {
             if(result) {
@@ -55,176 +51,77 @@ router.get('/:bookId', (req, res, next) => {
         });
 });
 
-
-//return books of an author: VERRRRRRRR
-
-router.get('/author/:id', (req, res, next) => {
-    let idAuthor = req.params.id;
-
-    Book.find({ "author" : idAuthor })
-        .select('isbn title author description')
-        .exec()
-        .then(bookCollection => {
-            const response = {
-                count: bookCollection.length,
-                books: bookCollection
-            }
-
-            if (response.count) {
-                res.status(200).json(response);
-            } else {
-                res.status(200).json({
-                    message: 'This author does not have any book'
-                });
-            }
-        })
-        .catch(err => {
-                res.status(500).json({
-                    error: err
-                });
-        });
-});
-
-
-//return books of an specific genre
-
-router.get('/genre/:genre', (req, res, next) => {
-    let genre = req.params.genre;
-
-    Book.find({ genre: genre })
-        .select('isbn title author description')
-        .exec()
-        .then(bookCollection => {
-            const response = {
-                count: bookCollection.length,
-                books: bookCollection
-            }
-
-            if (response.count) {
-                res.status(200).json(response);
-            } else {
-                res.status(200).json({
-                    message: 'This genre does not have any book'
-                });
-            }
-        })
-        .catch(err => {
-                res.status(500).json({
-                    error: err
-                });
-        });
-});
-
-
-//post a book
-
 router.post('/new', (req, res, next) => {
-    let isbn = req.body.isbn;
-    let title = req.body.title;
-    let author = req.body.author;
-    let description = req.body.description;
-    let image = req.body.image;
-    let genre = req.body.genre;
-    //let reviews = req.body.reviews;
-    //let quotes = req.body.quotes;
 
     var book = new Book({
-        isbn: isbn,
-        title: title,
-        author: author,
-        description: description,
-        image: image,
-        genre: genre
-        //reviews: reviews,
-        //quotes: quotes
+        isbn: req.body.isbn,
+        title: req.body.title,
+        author: {
+            name: req.body.name,
+            surname: req.body.surname
+        },
+        description: req.body.description,
+        image: req.body.image,
+        genre: req.body.genre,
     });
 
     book.save()
         .then(result => {
-            console.log(result);
             res.status(201)
-               .json({
-                   message: "Book created successfully",
-                   createdBook: {
-                       _id: result._id,
-                       title: result.title
-                   }
-               });
+                .json({
+                    message: "Book created successfully",
+                    createdBook: {
+                        _id: result._id,
+                        title: result.title,
+                    }
+                });
         })
         .catch(err => {
             res.status(500)
-               .json({
-                   error: err
-               });
+                .json({
+                    error: err
+                });
         });
 });
 
-
-//update a book: VER
-
-router.patch('/:id', (req, res, next) => {
-    const id = req.params.id;
-  
-    const updateArray = {}
-  
-    for (const ops of req.body) {
-      updateArray[ops.change] = ops.value;
-    }
-  
-    Book.update({ _id: id }, { $set: updateArray })
-      .exec()
-      .then(result => {
-        if (result.nModified) {
-          res.status(200).json({
-            status: result.ok,
-            changed: result.nModified,
-            message: 'Book updated successfully'
-          });
+router.post('/:id/quote', (req, res, next) => {
+    Book.findByIdAndUpdate(
+        req.params.id,
+        { $push: { "quotes": { quote: req.body.quote } } },
+        { safe: true, upsert: true }
+    )
+    .then(res.status(201).json(
+        {
+            message: "Quote added successfully"
         }
-        else {
-          res.status(200).json({
-            status: result.ok,
-            changed: result.nModified,
-            message: 'No attributes were affected. Please check change and value attributes of your request.'
-          });
-        }
-  
-      })
-      .catch(err => {
+    ))
+    .catch(err => {
         res.status(500).json({
-          error: err
+            error: err
         });
-      });
-  });
+    });
+});
 
-
-//delete a book
-
-router.delete('/:id', (req, res, next) => {
-    const id = req.params.id;
-    Book.remove({ _id: id })
-      .exec()
-      .then(result => {
-  
-        const response = {
-          count: result.result.n,
-          status: result.result.ok,
-          message: 'Book deleted successfully'
-        }
-  
-        if (result.result.n) {
-          res.status(200).json(response);
-        }
-        else {
-          response.message = 'No book was deleted';
-          res.status(200).json(response);
-        }
-      })
-      .catch(err => {
-        res.status(500).json({
-          error: err
+router.post('/:id/review', (req, res, next) => {
+    Book.findByIdAndUpdate(
+        req.params.id,
+        { $push: { "reviews": { 
+            stars: req.body.stars,
+            comment: req.body.comment,
+            user: req.body.user 
+        } } },
+        { safe: true, upsert: true }
+    )
+        .then(res.status(201).json(
+            {
+                message: "Review added successfully"
+            }
+        ))
+        .catch(err => {
+            res.status(500).json({
+                error: err
+            });
         });
-      });
 });
 
 
